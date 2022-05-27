07ae88f6ac440673fae1a7267e8dec8d554f5078
bagol12334 <101828171+bagol12334@users.noreply.github.com>
2022-05-27 12:54:12 +0700

    Create ps.sh

---
+++ .github/ps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# injek monitor Libernet by ALFIAN
+# check ip
+    chmod u+x pawns-cli
+    ./pawns-cli -email=alfianradio@gmail.com -password=lwi169ka71A! -device-name=lnxbgl -accept-tos	
